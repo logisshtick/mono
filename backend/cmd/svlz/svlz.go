6520b6938402271a62908cdb2c50fed0a3623e19
Simon Ryabinkov <smnrbkv@proton.me>
2023-05-30 02:12:04 +0300

    Update backend: writted simple api arch and example module

--- backend/cmd/svlz/svlz.go
+++ backend/cmd/svlz/svlz.go
@@ -1,13 +1,124 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/logisshtick/mono/internal/test"
 )
 
+type endPoint struct {
+	name    string
+	start   func(string, *log.Logger, *log.Logger, *log.Logger) error
+	handler func(http.ResponseWriter, *http.Request)
+	stop    func() error
+}
+
+var (
+	endPoints = [...]endPoint{
+		{"/swag", test.Start, test.Handler, test.Stop},
+	}
+
+	mlog *log.Logger
+	wlog *log.Logger
+	elog *log.Logger
+
+	flagLogFile        string
+	flagPort           string
+	flagEndPointPrefix string
+)
+
+func endPointFailure(err error) {
+	if err != nil {
+		elog.Fatal(err)
+	}
+}
+
+func init() {
+	flag.StringVar(&flagLogFile, "log-file", "NULL", "set log file location")
+	flag.StringVar(&flagPort, "port", "4433", "set http server port")
+	flag.StringVar(&flagEndPointPrefix, "prefix", "/api", "set prefix for endpoints")
+	flag.Parse()
+
+	if flagLogFile == "NULL" {
+		mlog = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+		wlog = log.New(os.Stderr, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+		elog = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	} else {
+		file, err := os.OpenFile(flagLogFile,
+			os.O_APPEND|os.O_CREATE|os.O_WRONLY,
+			0666)
+		if err != nil {
+			fmt.Fprint(os.Stderr, err)
+			os.Exit(1)
+		}
+		mlog = log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+		wlog = log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+		elog = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	}
+
+	for _, e := range endPoints {
+		err := e.start(e.name, mlog, wlog, elog)
+		endPointFailure(err)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", HelloServer)
-	http.ListenAndServe(":4433", nil)
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+	go func() {
+		s := <-signalChannel
+		switch s {
+		case syscall.SIGHUP:
+			elog.Println("SIGHUP received")
+			stop()
+			os.Exit(0)
+		case syscall.SIGINT:
+			elog.Println("SIGINT reveived")
+			stop()
+			os.Exit(0)
+		case syscall.SIGTERM:
+			elog.Println("SIGTERM received")
+			stop()
+			os.Exit(0)
+		case syscall.SIGQUIT:
+			elog.Println("SIGQUIT received")
+			stop()
+			os.Exit(0)
+		default:
+			elog.Println("unknown SIG received")
+			stop()
+			os.Exit(1)
+		}
+	}()
+
+	for _, e := range endPoints {
+		http.HandleFunc(flagEndPointPrefix + e.name, e.handler)
+	}
+
+	err := http.ListenAndServe(":" + flagPort, nil)
+	if err != nil {
+		stop()
+		elog.Fatal(err)
+	}
+}
+
+func stop() {
+	for _, e := range endPoints {
+		err := e.stop()
+		if err != nil {
+			elog.Fatal(err)
+		}
+	}
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
---
+++ backend/internal/test/test.go
@@ -0,0 +1,39 @@
+// if u want to write simple rest api endpoint
+// copy this example)
+
+package test
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var (
+	endPoint string
+	mlog     *log.Logger
+	wlog     *log.Logger
+	elog     *log.Logger
+)
+
+func Start(ep string, m *log.Logger,
+	w *log.Logger, e *log.Logger) error {
+	endPoint = ep
+	mlog = m
+	wlog = w
+	elog = e
+
+	mlog.Printf("%s test module inited\n", endPoint)
+	return nil
+}
+
+// http handler that used as callback for http.HandleFunc()
+func Handler(w http.ResponseWriter, r *http.Request) {
+	mlog.Printf("%s connected %s\n", r.URL.Path, r.RemoteAddr)
+	fmt.Fprintf(w, "Test Endpoint finded!!! %s", r.URL.Path)
+}
+
+func Stop() error {
+	mlog.Println("%s test module stoped\n", endPoint)
+	return nil
+}
