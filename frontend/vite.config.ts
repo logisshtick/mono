88d5530089d08b3a6e8b420b68a848ff870a24ad
Arsenii Izmailov <67082372+Turbocat6413@users.noreply.github.com>
2023-05-31 00:50:24 +0300

    Update front: added zlib and brotli

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import viteCompression from 'vite-plugin-compression';
+import zlib from 'zlib';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [
+    preact(),
+    viteCompression({
+      algorithm: 'brotliCompress',
+      filter: /\.(js|mjs|json|txt|css|html|svg|woff2)$/i,
+      compressionOptions: {
+        params: {
+					[zlib.constants.BROTLI_PARAM_QUALITY]: zlib.constants.BROTLI_MAX_QUALITY,
+          [zlib.constants.BROTLI_PARAM_MODE]: zlib.constants.BROTLI_MODE_TEXT,
+				},
+      }
+    })
+  ],
 })
