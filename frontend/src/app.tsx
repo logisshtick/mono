e78d7417fcb6593367e9588d9508b5dfa31100f9
Arsenii Izmailov <67082372+Turbocat6413@users.noreply.github.com>
2023-06-10 11:12:35 +0300

    Update front: continued work on login flow

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -3,31 +3,37 @@ import { IntlProvider } from 'preact-i18n';
 import { useEffect, useState } from 'preact/hooks';
 
 import './global.css';
+import './fonts.css'
 
 import Home from './routes/Home';
 import Login from './routes/Login';
 
 import Header from './components/Header';
+import Splash from './components/Splash';
 
  
 export function App() {
 	const [definition, setDefinition] = useState({});
+	//const [splash, setSplash] = useState(true);
 
 	async function fetchLocale() {
-		const response = await fetch(`/i18n/en-US.json`);
+		const response = await fetch(`/i18n/pl-PL.json`);
 		const localeStrings = await response.json();
 
 		setDefinition(localeStrings);
 	}
 
 	useEffect(() => {
-		
-
 		fetchLocale();
+
+		//setTimeout(() => setSplash(false), 1000);
+
+		
 	}, []);
 
 	return (
 		<IntlProvider definition={definition}>
+			<Splash />
 			<main >
 				<Header />
 				<Router>
