953cd9843a07fa1442da1171b4b984639629b8b5
Arsenii Izmailov <67082372+Turbocat6413@users.noreply.github.com>
2023-07-15 03:32:52 +0300

    Update front: іди нахуй

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,34 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import viteCompression from 'vite-plugin-compression';
+import eslint from 'vite-plugin-eslint'
 import zlib from 'zlib';
+import { VitePWA } from 'vite-plugin-pwa'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    eslint(),
     preact(),
+    VitePWA({
+      manifest: {
+        name: 'SVLZ',
+        short_name: 'SVLZ',
+        id: '/',
+        start_url: '/?from=homescreen',
+        display: 'standalone',
+        orientation: 'portrait-primary',
+        lang: 'en',
+        dir: 'ltr',
+        display_override: [
+          'standalone',
+          'minimal-ui',
+          'window-controls-overlay'
+        ],
+        prefer_related_applications: false,
+      }
+    }),
     viteCompression({
       algorithm: 'brotliCompress',
       filter: /\.(js|mjs|json|txt|css|html|svg|woff2)$/i,
